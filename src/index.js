0e0222fae6c0044f31a30dff8e2c3d68258f1316
Anton Safronov <safer_bwd@mail.ru>
2024-03-19 18:08:53 +0300

    style(debug)

--- src/index.js
+++ src/index.js
@@ -356,7 +356,7 @@ class Connection extends EventEmitter {
       }
     }
 
-    debug('[%s] amqp: connected (id = %s)', this.applicationId, this._connection.id);
+    debug('[%s] amqp: connected (%s)', this.applicationId, this._connection.id);
 
     // set reconnect
     if (this._options.amqp.reconnect) {
